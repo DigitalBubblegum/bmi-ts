07df745ed1d80318bad3ecc7745d8bcb3423abdf
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-23 15:03:07 +0530

    -completed exercise 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,31 +1,34 @@
-const calculateBmi = (h:number,w:number)=> {
+export const calculateBmi = (h:number,w:number):string => {
     const value = (w /((h*0.01)*(h*0.01)))
+    var result = 'invalid'
     if (value < 16.0) {
-        console.log("Underweight (Severe thinness)");
+        result = "Underweight (Severe thinness)"
     }
     else if (value >= 16.0 && value <= 16.9) {
-        console.log("Underweight (Moderate thinness)");
+        result = "Underweight (Moderate thinness)	"
     }
     else if (value >= 17.0 && value <= 18.4) {
-        console.log("Underweight (Mild thinness)");
+        result = "Underweight (Mild thinness)"
     }
     else if (value >= 18.5 && value <= 24.9) {
-        console.log("Normal range");
+        result = "Normal range"
     }
     else if (value >= 25.0 && value <= 29.9) {
-        console.log("Overweight (Pre-obese)");
+        result = "Overweight (Pre-obese)"
     }
     else if (value >= 30.0 && value <= 34.9) {
-        console.log("Obese (Class I)");
+        result = "Obese (Class I)"
     }
     else if (value >= 35.0 && value <= 39.9) {
-        console.log("Obese (Class II)");
+        result = "Obese (Class II)"
     }
     else if (value >= 40.0) {
-        console.log("Obese (Class III)");
+        result = "Obese (Class III)"
     }
+
+    return result
 }
 
 const h: number = Number(process.argv[2])
 const v: number = Number(process.argv[3])
-calculateBmi(h,v)
+console.log(calculateBmi(h,v))
--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+// import { calculateBmi } from './bmiCalculator';
 const app = express();
 app.get('/hello',(_req,res)=>{
     res.send('Hello Full Stack!')
+})
+
+app.get(`/bmi`,(req,res)=>{
+    const userObj = req.query
+    const height:number = Number(userObj.height);
+    const weight:number = Number(userObj.weight); 
+    console.log(height,weight)
+    const userRes = {
+        height: height,
+        weight: weight,
+        bmi: calculateBmi(height,weight)
+    }
+    res.send(userRes)
 })
 
 const PORT = 3000
